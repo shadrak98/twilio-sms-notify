2d296af3dde1a90e2b495eb86bfc316cd83e36a0
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2024-01-25 12:55:09 +0530

    feat: Twilio Configuration

---
+++ twilio_sms_notify/api/twilio_client.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+import frappe
+
+AUTH_TOKEN = frappe.db.get_single_value("Twilio Settings", "auth_token")
+ACCOUNT_SID = frappe.db.get_single_value("Twilio Settings", "account_sid")
+FROM = frappe.db.get_single_value("Twilio Settings", "from_number")
+
+@frappe.whitelist()
+def send_sms(to, body):
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = client.messages.create(
+        to=to,
+        from_=FROM,
+        body=body
+    )
+    return message.sid
---
+++ twilio_sms_notify/twilio_sms_notify/doctype/twilio_settings/test_twilio_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, shadrak and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTwilioSettings(FrappeTestCase):
+	pass
